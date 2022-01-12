e67f5efb6b9c06c898baa7b1dc38d48c57f3bc1e
sseonxx <you@example.com>
2022-01-12 16:57:55 +0900

    First Commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://hr:hr@seoeunplate.wxv5k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+).then(() => console.log('MongoDB connected!'))
+  .catch(err => console.log(err))
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+/*
+mongodb+srv://hr:<password>@seoeunplate.wxv5k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+
+*/
