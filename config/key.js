bfcc7a121483346fdb88064ef990d9251f99080f
sseonxx <you@example.com>
2022-01-20 16:52:49 +0900

    env 추가설명

--- config/key.js
+++ config/key.js
@@ -1,4 +1,4 @@
-if(process.env.NODE_ENV === 'production'){
+if(process.env.NODE_ENV === 'production'){ //npm install dotenv 해야 env파일 접근가능
     module.exports = require('./prod');
 }else{
     module.exports = require('./dev');
