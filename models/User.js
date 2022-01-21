2ef7d4af3c1e6b040f7d6c58c77d9191f37e23fe
sseonxx <you@example.com>
2022-01-21 14:45:57 +0900

    crypt 해시

--- models/User.js
+++ models/User.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
 name: {
@@ -30,5 +32,24 @@ tokenExp: {
 })
 
 const User = mongoose.model('User',userSchema)
+
+userSchema.pre('save',function( next ){ // 유저 정보를 register하기 전에 function()수행
+    var user = this;
+    //비밀번호를 암호화 시킨다.
+    if(user.isModified('password')){
+        
+        bcrypt.genSalt(saltRounds,function(err,salt){
+            if (err) return next(err)
+            
+            bcrypt.hash(user.password, salt, function(err,hash){
+                if (err) return next(err);
+                user.password = hash
+                next()
+            })
+        })
+    }
+
+
+})
 
 module.exports = { User   }
